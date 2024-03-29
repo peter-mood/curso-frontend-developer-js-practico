5e1be5a40c9289ca572d600b5d797c91c7cf6b2a
Peter Anzulez <peteranzulezhinojosa@gmail.com>
2024-03-28 22:17:12 -0500

    Se agrego el slider de manu carrito aun falta menjorar

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ const desktopMenu   = document.querySelector('.desktop-menu');
 /*menu de mobile */
 const menuHamIcon   = document.querySelector('.menu');
 const menuMobile    = document.querySelector('.mobile-menu');
+/*desplazo de carrito  */
+const iconCarrito   = document.querySelector('.navbar-shopping-cart');
+const menuCarrito   = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu );
+iconCarrito.addEventListener('click',toogleMenuCarrito );
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 function toogleMobileMenu() {
     menuMobile.classList.toggle('inactive');
+}
+function toogleMenuCarrito() {
+    menuCarrito.classList.toggle('inactive');   
+    
 }
