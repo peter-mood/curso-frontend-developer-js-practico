853f06ac7725f2764cf25e27f15b30f247b46a86
Peter Anzulez <peteranzulezhinojosa@gmail.com>
2024-03-28 21:53:45 -0500

    Se agrega menu responsive y menu de sesion

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail     = document.querySelector('.navbar-email');
+const desktopMenu   = document.querySelector('.desktop-menu');
+/*menu de mobile */
+const menuHamIcon   = document.querySelector('.menu');
+const menuMobile    = document.querySelector('.mobile-menu');
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu );
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toogleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
